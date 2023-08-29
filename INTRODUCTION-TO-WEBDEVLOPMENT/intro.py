7fe242648ead465d91e275f36dfe28ea1f565c18
[harish saifi] <harishsaifi2003@gmail.com>
2023-08-29 20:48:04 +0530

    full-stack-webdevlopment-course

---
+++ INTRODUCTION-TO-WEBDEVLOPMENT/intro.py
@@ -0,0 +1,13 @@
+# hey welcome to course of full stack devlopment
+# in this modeule we will learn abot web devlopment
+#
+
+
+# excited to the new journey 
+
+# this module is on about INtro to web devlopment
+
+print("welcome to New Journey")
+
+
+#1.  
